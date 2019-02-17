e87a8edc84952e9e873086a3dbb4fea5a0f172d9
Dziugas Butkus <dziubutkus@gmail.com>
2019-02-17 02:30:36 -0700

    fix portis, pay per play

--- js/core.js
+++ js/core.js
@@ -1,84 +1,85 @@
-var mockAddress = "0x0f138740e225f0DA140e32F1594DD4257CEaf036";
+var mockAddress = "0xEc301A8516e5FadE516e01929d56E30a9CfbD8ae";
 var mockContract = null;
 
 // Declare web3 library
 //if (typeof web3 !== 'undefined') {
-//    web3 = new Web3(web3.currentProvider);
+//web3 = new Web3(web3.currentProvider);
 //} else {
-//    web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/ef4aee25daf64b56904871052a066b2b"));
+// web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/ef4aee25daf64b56904871052a066b2b"));
 //}
-console.log('hello1')
 
-const portis = new Portis('77564852-ff04-44f9-96f8-568084ee641d', 'rinkeby', {scope: ["email"]});
+
+const portis = new Portis('77564852-ff04-44f9-96f8-568084ee641d', 'rinkeby');
 const web3 = new Web3(portis.provider);
+portis.provider.enable().then(console.log)
 
-portis.provider.enable().then(function(accounts) {
-	console.log(accounts[0])
-})
-
+let defaultAccount;
 portis.onLogin((walletAddress, email) => {
-  console.log(walletAddress, email);
+    $('.w_address').html(walletAddress);
+    defaultAccount = walletAddress;
 });
 
 
 // Checking if user have existing web3 provider
 if (typeof web3 !== 'undefined') {
-    //web3.eth.defaultAccount = web3.currentProvider.selectedAddress;
-
-    // Join Smart Contract
-    //var mockABI = $.getJSON("json/MockABI.json", function(contractABI) {
-        //window.lotteryContract = new web3.eth.Contract(contractABI, window.mockAddress);
-        // window.lotteryContract = lotteryContractABI.at(window.lotteryAddress);
-    //});
+    web3.eth.defaultAccount = web3.currentProvider.selectedAddress;
+    console.log("lox");
+    var mockABI = $.getJSON("json/MockABI.json", function(contractABI) {
+        window.radioContract = new web3.eth.Contract(contractABI, window.mockAddress);
+        //window.lotteryContract = lotteryContractABI.at(window.lotteryAddress);
+    });
 
 }
 
 // Website on load processes
 jQuery(document).ready(function ($) {
     "use strict";
-    // Add information in website from MetaMask
-	console.log(typeof web3);
-    if (typeof web3 !== 'undefined') {
 
-        setTimeout(function () {
-            $('.w_address').html(web3.currentProvider.selectedAddress);
-            web3.eth.getBalance(web3.currentProvider.selectedAddress, function (err, resp) {
-                var balance = web3.utils.fromWei(resp, 'ether');
-                $('.w_balance').html(balance + ' ETH');
+    $('#jp-play').click(function() {
+        console.log(defaultAccount);
+        window.radioContract.methods.listen().send({from: defaultAccount}, function(err, resp) {
+            console.error(err);
+            console.warn(resp);
+            console.log("dsankjfhsakjfn")
+
+        })
+            .on('transactionHash', function (hash) {
+                console.log("asdasklflaskfmklamflkasmf")
+                $("#jquery_jplayer_1").jPlayer({
+                    ready: function () {
+                        $(this).jPlayer("setMedia", {
+                            mp3: "media/insomniac_olympics.flac",
+                            oga: "audio/TEDxPhoenix-KelliAnderson-DisruptiveWonderforaChange.ogg"
+                        });
+                    },
+                    swfPath: "./js",
+                    supplied: "mp3,oga"
+                });
+
+            })
+            .on('receipt', function (receipt) {
+                /* jPlayer! */
+
             });
-        }, 50);
-    } else {
-        $('.metamask-info').hide();
-        $('.w_address, .w_balance').html('Error');
-    }
-	/* jPlayer! */
-	$("#jquery_jplayer_1").jPlayer({
-		ready: function () {
-		  $(this).jPlayer("setMedia", {
-			mp3: "media/insomniac_olympics.flac",
-			oga: "audio/TEDxPhoenix-KelliAnderson-DisruptiveWonderforaChange.ogg"
-		  });
-		},
-		swfPath: "./js",
-		supplied: "mp3,oga"
-	});
+    });
+
+    $(".portis-button").click(function() {
+        $("#jquery_jplayer_1").jPlayer("play");
+    })
+
+
+    /*
+    $("#jquery_jplayer_1").jPlayer({
+                ready: function () {
+                    $(this).jPlayer("setMedia", {
+                        mp3: "media/insomniac_olympics.flac",
+                        oga: "audio/TEDxPhoenix-KelliAnderson-DisruptiveWonderforaChange.ogg"
+                    });
+                },
+                swfPath: "./js",
+                supplied: "mp3,oga"
+            });
+     */
 	
 });
 
-function timeConverter(UNIX_timestamp) {
-    var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes();
-    var sec = a.getSeconds();
-
-    if (hour < 10) 	hour = '0'+hour;
-    if (min < 10) 	min = '0'+min;
-    if (sec < 10) 	sec = '0'+sec;
-
-    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-    return time;
-}
