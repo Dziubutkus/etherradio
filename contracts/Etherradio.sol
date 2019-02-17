a6b9997cd3bad0d5e258eadd7b0963875253c230
Dziugas Butkus <dziubutkus@gmail.com>
2019-02-16 17:14:20 -0700

    finished withdraw function, mapped address to an array of songs

--- contracts/Etherradio.sol
+++ contracts/Etherradio.sol
@@ -6,39 +6,70 @@ import "./SafeMath.sol";
 contract Etherradio is Ownable {
     using SafeMath for uint;
 
-
     // Total ether collected
     uint bank;
+    // Subscription price
+    uint subscriptionPrice = 1 ether;
     // Array of artists
     address payable[] public artists;
-    mapping (string => mapping (address[] => uint[]));
-    mapping (address => bool) public allowedUsers;
+    mapping (address => bool) public allowedArtists;
+    mapping (address => bool) public subscribedUsers;
+    //mapping (address => Song) public songsToAddress;
+    mapping (address => mapping (uint => Song)) public addressToSongs;
+    uint numberOfSongs;
 
+    struct Song {
+        address songOwner;
+        string songName;
+        address payable[] artists;
+        uint[] royalties;
+    }
 
+    Song[] songs;
 
-    string public songName;
-    mapping (address => uint) public royalties;
-    uint public moneyCollected;
+    event AddedSong(string _songName);
+    event NewSubscriber(address subscriber);
 
-    constructor(string memory _songName, address payable[] memory _artists, uint[] memory _royalties) public {
-        songName = _songName;
-        artists = _artists;
-        for(uint i = 0; i < _artists.length; i++) {
-            royalties[_artists[i]] = _royalties[i];
-            allowedUsers[_artists[i]] = true;
-        }
+    constructor() public {
+
     }
 
     function () external payable {
-        require(msg.value > 0);
-        moneyCollected = moneyCollected.add(msg.value);
+        subscription();
     }
 
-    function withdrawAll() public {
-        require(allowedUsers[msg.sender]);
-        uint moneyToTransfer = moneyCollected.mul(royalties[msg.sender]).div(100);
-        msg.sender.transfer(moneyToTransfer);
+    function subscription() public payable {
+        require(msg.value == subscriptionPrice, "Not enough ether to buy a subscription");
+        require(!subscribedUsers[msg.sender], "You have already subscribed");
+        bank = bank.add(msg.value);
+        subscribedUsers[msg.sender] = true;
+
+        emit NewSubscriber(msg.sender);
     }
-*/
+
+    function addSong(string memory _songName, address payable[] memory _artists, uint[] memory _royalties) onlyOwner public {
+        Song storage songsTemp = songs[numberOfSongs];
+        songsTemp.songOwner = msg.sender;
+        songsTemp.songName = _songName;
+        songsTemp.artists = _artists;
+        songsTemp.royalties = _royalties;
+        songs.push(songsTemp);
+
+        //songsToAddress[msg.sender] = songsTemp;
+        addressToSongs[msg.sender][numberOfSongs] = songsTemp;
+        numberOfSongs++;
+
+        emit AddedSong(songsTemp.songName);
+    }
+
+    function withdraw(uint _songId) public {
+        require(songs[_songId].songOwner == msg.sender, "You are not the song owner");
+        Song memory songTemp = songs[_songId];
+        uint amountToDistribute = 1 ether;
+        for(uint i = 0; i < songTemp.artists.length; i++) {
+            uint forArtist = amountToDistribute.mul(songTemp.royalties[i]).div(100);
+            songTemp.artists[i].transfer(forArtist);
+        }
+    }
 
 }
