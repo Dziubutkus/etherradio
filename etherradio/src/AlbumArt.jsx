9ec067130afd97288cb146e9a93469a9630afac4
vincent <elysiumzero117@gmail.com>
2020-02-15 22:00:25 -0700

    centering and grid

--- etherradio/src/AlbumArt.jsx
+++ etherradio/src/AlbumArt.jsx
@@ -2,13 +2,22 @@ import React, { Component } from "react";
 import albumCover from "./media/vultures.png";
 // We almost just want this to be passed in a image object or url and then send out an http request to 
 // go retreve and render. In the encap player component. 
+const styles = {
+    center: {
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  }
 class AlbumArt extends Component {
 
     render() {
         const { image } = this.props;
         console.log(image);
         return(
-            <img id="cover-art" src={albumCover} alt="album cover art"></img>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <img id="cover-art" src={albumCover} alt="album cover art"></img>
+            </div>
+            
         )
     }
 }
--- etherradio/src/App.js
+++ etherradio/src/App.js
@@ -6,23 +6,40 @@ import Background from './Background'
 import logo from './logo.svg';
 import AlbumArt from './AlbumArt'
 import './style.css';
-
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
 function App() {
-
-
-
+  const [spacing, setSpacing] = React.useState(2);
+  const classes = useStyles();
   return (
     <div className="App">
       <header className="App-header">
           <h1>ETHERRADIO</h1>
 			    <ul id="links"><li><a href="#" id="exchange">Exchange</a></li></ul>
-          <img src={logo} className="App-logo" alt="logo" />
-          <AlbumArt image='./media/vultures.png'/>
+          
       </header>
-
+      
       <body>
-        <Background />
-        
+      <Grid container className={classes.root} spacing={2}>
+        <Grid item xs={12}>
+          <Grid container justify="center" spacing={spacing}>
+            <AlbumArt image='./media/vultures.png'/>
+            <Background />
+          </Grid>
+        </Grid>
+      </Grid>
       </body>
     </div>
   );
