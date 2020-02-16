372496100ca284e23eaed59bfebec85910006d68
Casey Key <caseykey@rams.colostate.edu>
2020-02-15 18:27:11 -0700

    added stylesheet from og

--- etherradio/src/App.js
+++ etherradio/src/App.js
@@ -3,25 +3,17 @@
 // navbar, tip, and music player components
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        <p>background here</p>
+      </body>
     </div>
   );
 }
